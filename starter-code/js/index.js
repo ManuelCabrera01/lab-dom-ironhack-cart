4f518725e9ed6092648d073aa67ab02ec4c0ce94
Manuel <m.a.c.p90@gmail.com>
2018-04-15 10:59:26 -0400

    first iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,18 +1,39 @@
- //declaration area
+//declaration area
 const getId = id => document.getElementById(id);
 
-const getClass = classes =>  document.getElementsByClassName(classes);
+const getClass = classes => document.getElementsByClassName(classes);
 
 const getByTN = tagName => document.getElementsByTagName(tagName);
 
 // end of declaration area
 
+// getting the price and turning it into a num
+let getItemPrice = getByTN("span")[1].innerHTML;
+//my price
+const itemPrice = parseInt(getItemPrice.replace(/[^0-9\.-]+/g, ""));
 
+//calculate button
+let calculateButton = getClass("btn")[1];
+
+calculateButton.onclick = () => {
+  // getting the value of the QTY
+  let getInput = getByTN("input")[0];
+  let getInputValue = getInput.value;
+  //calculating the price
+  let calculatePrice = itemPrice * getInputValue;
+  price = "$" + calculatePrice;
+
+  //Updates the total price in the DOM
+  let TotalPrice = (getByTN("span")[2].innerText = price);
+
+  console.log(TotalPrice);
+};
+let addItemName, addItemPrice, AddItemQty, addItemTPrice, addDeleteBottun;
+// TotalPrice = text;
 
 // function deleteItem(e){
 
 // }
-
 // function getPriceByProduct(itemNode){
 
 // }
